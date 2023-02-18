ce99ce6bc262ece00138209911a18b297a25f4a7
khawki00 <khawki00@hotmail.com>
2023-02-18 07:52:59 -0500

    Adding navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Hello World! We will add navbar!</h1>
+      <h1>test</h1>
     </div>
   )
 }
